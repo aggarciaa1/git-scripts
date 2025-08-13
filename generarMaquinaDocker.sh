a4cf2912e2e99a8275e97e98bb4ed7d1a25eba56
Allan Gabriel Garcia Aguilar <aggarciaa@bancatlan.hn>
2025-08-13 20:08:46 +0000

    generar maquinas docker cambios

--- generarMaquinaDocker.sh
+++ generarMaquinaDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+docker network rm ansible
 docker network create ansible --subnet=192.186.1.0/24
-docker run --detach --previleged --volume=/sys/fs/cgroup:/sys/fs/cgroup:rw --ip 192.168.1.10 --cgroups=host --name=debian1 --network=ansible apasoft/debian11-ansible
-docker run --detach --previleged --volume=/sys/fs/cgroup:/sys/fs/cgroup:rw --ip 192.168.1.11 --cgroups=host --name=ubuntu1 --network=ansible apasoft/ubuntu22-ansible
+docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:rw --ip 192.168.1.10 --cgroupns=host --name=debian1 --network=ansible apasoft/debian11-ansible
+docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:rw --ip 192.168.1.11 --cgroupns=host --name=ubuntu1 --network=ansible apasoft/ubuntu22-ansible
